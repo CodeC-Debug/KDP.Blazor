509b360fa47f611927b0122c80939cec9029c2fe
Artur Burghardt <ab@sander365.de>
2022-04-18 12:32:16 +0200

    Projektdateien hinzufügen.

---
+++ KDP.Blazor/DataObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KDP.Blazor
+{
+    public class DataObject
+    {
+        public string Word { get; set; }
+        public int Count { get; set; }
+    }
+}
